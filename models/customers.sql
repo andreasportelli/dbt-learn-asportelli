b51a1f9b5de7ef9403de802853cce502362001d7
Andrea Sportelli <Andrea.Sportelli@dunelm.com>
2020-11-10 13:23:13 +0000

    lifetime value

--- models/customers.sql
+++ models/customers.sql
@@ -4,22 +4,29 @@ with customers as (
 
 ),
 
-orders as (
+stg_orders as (
 
     select * from {{ ref('stg_orders') }}
+
+),
+
+orders as (
+
+    select * from {{ ref('orders') }}
 
 ),
 
 customer_orders as (
 
     select
-        customer_id,
-
+        so.customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(o.order_id) as number_of_orders,
+        sum(o.amount) as lifetime_value
 
-    from orders
+    from stg_orders so 
+    join orders o on o.order_id=so.order_id 
 
     group by 1
 
@@ -34,6 +41,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
